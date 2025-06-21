47fcd97a74c6371b1c96886d4bf702a7569717a0
Alexandria156 <alexandria12young@gmail.com>
2025-06-21 05:03:10 -0400

    Added test media folder to start debugging usersinout

--- usersInput.py
+++ usersInput.py
@@ -13,9 +13,11 @@ def img_convert(file_name, convert_type):
         temp = file_name
         if temp is None or len(temp) != 2:
             return OSError
+
         input_file = temp[0] + "." + temp[1]
         output_file = temp[0] + "." + convert_type
         old_image = Image.open(input_file)
+
         if input_file is output_file:
             print("Same file type")
         else:
@@ -23,16 +25,17 @@ def img_convert(file_name, convert_type):
                 new_image.save(output_file)
             print("Converted ", input_file, " to ", output_file)
             return new_image
+
         print("Failed to convert ", input_file, " to ", output_file)
         return None
+
     except OSError as ose:
-        print(ose.errno)
+        print("Invalid file\n", ose.errno)
         return ose
 
-
-
 def main():
-    pass;
+    name_and_type = read_file_name()
+    new_img = img_convert(name_and_type, "png")
 
 if __name__ == '__main__':
     main()
