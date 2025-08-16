f3a8238b162222275b95b9166ca9c8c81f26e0ee
Alexandria 156 <asy2678@rit.edu>
2025-08-16 09:08:43 -0400

    Added comments to img conversion to file type

--- usersInput.py
+++ usersInput.py
@@ -13,25 +13,27 @@ def video_convert(file_name, convert_type):
             return OSError
         input_file = temp[0] + "." + temp[1]
         output_file = temp[0] + "." + convert_type
-        
+
     except OSError as ose:
         print("Invalid file\n", ose.errno)
-    
-    pass
+        return ose
 
-def img_convert(file_name, convert_type):
+def img_convert_file_type(file_name, convert_type):
     try:
         temp =  file_name
+        # Check to see if the file name is valid
         if temp is None or len(temp) != 2:
             return OSError
-
+        # Get the location of the input file and type
         input_file = temp[0] + "." + temp[1]
+        # Same location as input and different type
         output_file = temp[0] + "." + convert_type
         old_image = Image.open(input_file)
 
         if input_file is output_file:
             print("Same file type")
         else:
+            # Conversion
             with old_image as new_image:
                 new_image.save(output_file)
             print("Converted ", input_file, " to ", output_file)
@@ -39,14 +41,15 @@ def img_convert(file_name, convert_type):
 
         print("Failed to convert ", input_file, " to ", output_file)
         return None
-
+    # Catch the OSError to alert a invalid file
     except OSError as ose:
         print("Invalid file\n", ose.errno)
         return ose
 
 def main():
     name_and_type = read_file_name()
-    new_img = img_convert(name_and_type, "png")
+    new_img = img_convert_file_type(name_and_type, "png")
+    print("Done!")
 
 if __name__ == '__main__':
     main()
