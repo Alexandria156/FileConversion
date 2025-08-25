de21bddb6a7c78f4d80b4616053e30e3fb7413aa
Alexandria 156 <asy2678@rit.edu>
2025-08-25 11:26:00 -0400

    Image optimization does not work

--- usersInput.py
+++ usersInput.py
@@ -66,13 +66,19 @@ def img_optimization(file_name, new_size):
         # Check to see if the file name is valid
         if temp_name is None or len(temp_name) != 2:
             return OSError
+        # opening the file and reading out its size
         img_file = open(file_name)
         file_size = os.path.getsize(img_file)
+
         print("File size: ", file_size/100000, "mb\n")
+
         reduction = file_size - (new_size * 100000)
+        # truncate using difference of file size and asked size
         img_file.truncate(reduction)
+
         print("File is now ", new_size* 100000, "mb\n")
         return img_file
+    
     except OSError as ose:
         print("Invalid file\n", ose.errno)
         return ose
@@ -82,8 +88,9 @@ def img_optimization(file_name, new_size):
 
 def main():
     name_and_type = read_file_name()
-    #img_convert_file_type(name_and_type, "png")
-    video_convert_file_type(name_and_type, "wmv")
+    img_convert_file_type(name_and_type, "png")
+    img_optimization("./test_media/jp.png", .001)
+    #video_convert_file_type(name_and_type, "wmv")
     print("Done!")
 
 if __name__ == '__main__':
