16693f8a5b106e2e0e1afc7b5eefbbc9f8ada62e
Alexandria 156 <asy2678@rit.edu>
2025-08-25 18:36:39 -0400

    Resolved value error for image conversion method

--- conversions/imgConversion.py
+++ conversions/imgConversion.py
@@ -3,11 +3,13 @@ from PIL import Image
 def convertImage(file_name, convert_type):
     try:
         temp_name = file_name
-
         if temp_name is None or len(temp_name) !=2:
             return OSError
+        
+        # Get the location of the input file and type
         input_file = temp_name[0] + "." + temp_name[1]
-        output_file = temp_name[0] = "." + convert_type
+        # Same location as input and different type
+        output_file = temp_name[0] + "." + convert_type
         old_img = Image.open(input_file)
 
         if input_file is output_file:
@@ -21,9 +23,12 @@ def convertImage(file_name, convert_type):
             return 1
         print("Failed to convert ", input_file)
         return 0
+    
     except OSError as ose:
         print("Invalid file,", ose.errno, "\n")
         return ose
-
-def main():
-    convertImage()
+    '''
+    except ValueError as ve:
+        print("Failed to convert file,", ve.with_traceback,"\n")
+        return 0
+    '''
